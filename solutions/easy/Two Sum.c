58261d7569e434fc245ee1f75c5216dc1e57f60b
Hell..shubhamm.. <shubham79094@gmail.com>
2026-09-18 21:13:51 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.c
+++ solutions/easy/Two Sum.c
@@ -4,10 +4,11 @@
             // Link: https://leetcode.com/problems/two-sum/
 
 /**
- * Note: The returned array must be malloced, assume caller calls 
- free().
+ * Note: The returned array must be malloced, assume caller 
+ calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+int* twoSum(int* nums, int numsSize, int target, int* 
+returnSize) {
     for(int i = 0; i< numsSize; i++){
         for(int j = i + 1; j<numsSize; j++){
             if(nums[i] + nums[j] ==  target){
@@ -18,4 +19,3 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
                 return result;
             }
 
-        }
